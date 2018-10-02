62300167de8abc7172ba778017e1317e25666a91
Wassburgare <daniel.wassbjer@gmail.com>
2018-10-02 09:53:02 +0200

    Create backend queue

---
+++ backend/index.js
@@ -0,0 +1,98 @@
+const WebSocket = require('ws');
+const queue = require('./queue');
+
+const PORT = 8081;
+const INTERVAL = 10000;
+
+const JOIN_QUEUE = 'join_queue';
+const PLAY_INSTRUMENT = 'play_instrument';
+const QUEUE_UPDATED = 'queue_updated';
+
+const server = new WebSocket.Server({
+  port: PORT,
+}, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+let timerId;
+
+server.on('connection', (ws) => {
+  ws.on('message', (msg) => {
+    const message = JSON.parse(msg);
+
+    switch (message.type) {
+      case JOIN_QUEUE:
+        joinQueue(message.user, ws);
+        break;
+      case PLAY_INSTRUMENT:
+        if (isUserPlaying(queue.getUserFromWs(ws))) {
+          playInstrument(message);
+        }
+        break;
+      default:
+        break;
+    }
+  });
+
+  ws.on('close', () => {
+    if (queue.hasWs(ws)) {
+      leaveQueue(ws);
+    }
+  });
+});
+
+const joinQueue = (user, ws) => {
+  queue.addUser(user, ws);
+
+  if (queue.size() > 1 && !isTimerStarted()) {
+    startTimer();
+  }
+
+  informUsers();
+};
+
+const leaveQueue = (ws) => {
+  queue.removeUser(ws);
+
+  if (queue.size() <= 1 && isTimerStarted()) {
+    stopTimer();
+  }
+
+  ws.close();
+
+  informUsers();
+};
+
+const informUsers = () => {
+  queue.getAllWs().forEach((ws) => {
+    sendMessage(ws, QUEUE_UPDATED, queue.getAllUsers());
+  });
+};
+
+const playInstrument = (data) => {
+  // Do some stuff with the Thinger.io API
+};
+
+const startTimer = () => {
+  timerId = setInterval(setCurrentUser, INTERVAL);
+};
+
+const stopTimer = () => {
+  clearInterval(timerId);
+  timerId = undefined;
+};
+
+const isTimerStarted = () => timerId !== undefined;
+
+const setCurrentUser = () => {
+  leaveQueue(queue.peekWs());
+};
+
+const isUserPlaying = user => queue.compareUsers(user, queue.peekUser());
+
+const sendMessage = (ws, type, data) => {
+  ws.send(JSON.stringify({
+    type,
+    data,
+  }));
+};
---
+++ backend/queue.js
@@ -0,0 +1,54 @@
+const uuid = require('uuid/v4');
+
+const users = [];
+const wsToUser = new Map();
+const userToWs = new Map();
+
+const addUser = (user, ws) => {
+  users.push({ ...user, uuid: uuid() });
+  wsToUser.set(ws, user);
+  userToWs.set(user, ws);
+};
+
+const removeUser = (ws) => {
+  const user = wsToUser.get(ws);
+
+  users.splice(users.indexOf(user), 1);
+  wsToUser.delete(ws);
+  userToWs.delete(user);
+};
+
+const getAllWs = () => Array.from(userToWs.values());
+
+const getAllUsers = () => Array.from(wsToUser.values());
+
+const getWsFromUser = user => userToWs.get(user);
+
+const getUserFromWs = ws => wsToUser.get(ws);
+
+const hasWs = ws => wsToUser.has(ws);
+
+const hasUser = user => userToWs.has(user);
+
+const size = () => users.length;
+
+const peekWs = () => getWsFromUser(users[0]);
+
+const peekUser = () => users[0];
+
+const compareUsers = (u1, u2) => u1.uuid === u2.uuid;
+
+module.exports = {
+  addUser,
+  removeUser,
+  getAllWs,
+  getAllUsers,
+  hasWs,
+  hasUser,
+  getWsFromUser,
+  getUserFromWs,
+  peekWs,
+  peekUser,
+  compareUsers,
+  size,
+};
