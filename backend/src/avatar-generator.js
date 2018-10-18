7e382db39b45704dc0e5d62aa708eddb8ec2fd4b
Wassburgare <daniel.wassbjer@gmail.com>
2018-10-18 11:07:57 +0200

    Generate image when queue is empty

--- backend/src/avatar-generator.js
+++ backend/src/avatar-generator.js
@@ -6,17 +6,24 @@ const WebSocket = require('ws');
 const FRAMEBUFFER_DEVICE = '/dev/fb0';
 
 const IMG_DIR = path.resolve(__dirname, '..', '..', 'frontend', 'img');
+
 const AVATAR_LOCATION = path.resolve(__dirname, 'avatar.png');
 
 const SCREEN_WIDTH = 1920;
 const SCREEN_HEIGHT = 1080;
 
-const AVATAR_SCALE = 0.8;
 const AVATAR_SHAVE = 0.15;
-const AVATAR_CENTER_OFFSET = SCREEN_HEIGHT * 0.1;
+const AVATAR_SCALE = 0.8;
+const EMPTY_QUEUE_SCALE = 2;
 
-const USERNAME_SIZE = SCREEN_HEIGHT * 0.15;
-const USERNAME_CENTER_OFFSET = SCREEN_HEIGHT * 0.34;
+const IMAGE_CENTER_OFFSET = SCREEN_HEIGHT * 0.1;
+
+const USERNAME_FONT_SIZE = SCREEN_HEIGHT * 0.15;
+const EMPTY_QUEUE_FONT_SIZE = SCREEN_HEIGHT * 0.08;
+const TEXT_CENTER_OFFSET = SCREEN_HEIGHT * 0.34;
+
+// TODO: Set this to some better text
+const EMPTY_QUEUE_TEXT = 'To start playing, connect to dprj18 network\nand enter 192.168.1.234 in the browser!';
 
 const ws = new WebSocket('ws://localhost:8081');
 
@@ -30,8 +37,8 @@ ws.on('message', (message) => {
 
     mergeAvatar(`${IMG_DIR}/Eyes-${eyes}.svg`, `${IMG_DIR}/Mouth-${mouth}.svg`)
       .then(avatar => cropAvatar(avatar))
-      .then(avatar => extentAvatar(avatar))
-      .then(avatar => addUsername(avatar, nickname))
+      .then(avatar => extentImage(avatar))
+      .then(avatar => addText(avatar, nickname, USERNAME_FONT_SIZE))
       .then((avatar) => {
         gm(avatar)
           .write(AVATAR_LOCATION, (err) => {
@@ -40,7 +47,16 @@ ws.on('message', (message) => {
           });
       });
   } else {
-    // TODO: Don't display an avatar
+    emptyQueueImage()
+      .then(image => extentImage(image))
+      .then(image => addText(image, EMPTY_QUEUE_TEXT, EMPTY_QUEUE_FONT_SIZE))
+      .then((image) => {
+        gm(image)
+          .write(AVATAR_LOCATION, (err) => {
+            if (err) console.log(err);
+            displayImage();
+          });
+      });
   }
 });
 
@@ -64,29 +80,43 @@ const cropAvatar = avatar => new Promise((resolve, reject) => {
     });
 });
 
-const extentAvatar = avatar => new Promise((resolve, reject) => {
+const extentImage = avatar => new Promise((resolve, reject) => {
   gm(avatar)
     .gravity('Center')
-    .extent(SCREEN_WIDTH, SCREEN_HEIGHT, `+0+${AVATAR_CENTER_OFFSET}`)
+    .extent(SCREEN_WIDTH, SCREEN_HEIGHT, `+0+${IMAGE_CENTER_OFFSET}`)
     .toBuffer((err, buffer) => {
       if (err) reject(err);
       resolve(buffer);
     });
 });
 
-const addUsername = (avatar, username) => new Promise((resolve, reject) => {
+const addText = (avatar, username, fontSize) => new Promise((resolve, reject) => {
   gm(avatar)
-    .fontSize(USERNAME_SIZE)
-    .drawText(0, USERNAME_CENTER_OFFSET, username, 'Center')
+    .fontSize(fontSize)
+    .drawText(0, TEXT_CENTER_OFFSET, username, 'Center')
     .toBuffer((err, buffer) => {
       if (err) reject(err);
       resolve(buffer);
+    });
+});
+
+const emptyQueueImage = () => new Promise((resolve, reject) => {
+  gm(path.resolve(IMG_DIR, 'hands-on.png'))
+    .size(function getSize(sizeErr, size) {
+      if (sizeErr) reject(sizeErr);
+
+      this
+        .resize(size.width * EMPTY_QUEUE_SCALE, size.height * EMPTY_QUEUE_SCALE)
+        .toBuffer((bufferErr, buffer) => {
+          if (bufferErr) reject(bufferErr);
+          resolve(buffer);
+        });
     });
 });
 
 const displayImage = () => {
-  exec(`fbi -T 1 -d ${FRAMEBUFFER_DEVICE} --noverbose -a ${AVATAR_LOCATION}`, (err, stdout) => {
-    if (err) console.log(err);
-    if (stdout) console.log(stdout);
-  });
+  // exec(`fbi -T 1 -d ${FRAMEBUFFER_DEVICE} --noverbose -a ${AVATAR_LOCATION}`, (err, stdout) => {
+  //   if (err) console.log(err);
+  //   if (stdout) console.log(stdout);
+  // });
 };
