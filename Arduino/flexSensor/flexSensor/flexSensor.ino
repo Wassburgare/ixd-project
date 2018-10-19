4dd6408b81f3a58d1be4732755fe8e1e481bd9cc
akselbh <aksel@biornhansen.no>
2018-10-19 15:04:42 +0200

    arduino code for flex sensor

---
+++ Arduino/flexSensor/flexSensor/flexSensor.ino
@@ -0,0 +1,105 @@
+#define THINGER_SERVER "192.168.1.234"
+//This define has to be at the top of the document and contains the location of the thinger server
+
+//Sender represents a sensor sending data
+
+//Libraries for Wifi connectivity
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+//#include <ThingerWifi.h>
+#include <ThingerESP8266.h>
+
+//Login for Thinger server
+#define USER_ID             "Kloutschek"
+#define DEVICE_ID           "flexSensor" //FIXA! 
+#define DEVICE_CREDENTIAL   "!VwUoOsfblVi"
+
+const char *ssid = "dprj18";
+const char *password = "Dprj18iot";
+
+ThingerESP8266 thing(USER_ID, DEVICE_ID, DEVICE_CREDENTIAL);
+
+const int flexpin = 0; 
+int prevState = 0; 
+int curState = 0;
+int outPin = LED_BUILTIN;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+//Setting up Wifi
+  delay(1000);
+
+  //thing.add_wifi(ssid, password);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("success!");
+  Serial.print("IP Address is: ");
+  Serial.println(WiFi.localIP());
+
+  /* Sends random numbers to server
+    thing["send"] >> [](pson& out){
+    out = rand(); // or just a fixed number (out = 3;)
+    };*/
+}
+
+
+//Function to send message to another device
+void sendMessage() {
+  pson data;
+
+  //message is hej, can be changed, "instrument" is the reciever.
+  data["message"] = "Flex";
+  thing.call_device("controller", "receive" , data);
+  //thing.stream(thing["send"]);
+}
+
+void loop() {
+  //Fixes the thinger things
+  thing.handle();
+
+  
+  int flexposition;    // Input value from the analog pin.
+  int servoposition;   // Output value to the servo.
+
+  flexposition = analogRead(flexpin);
+
+  if (flexposition > 1000) {
+     curState = 1; 
+  }
+  else {
+     curState = 0;
+  }
+
+  if (curState != prevState){
+    prevState = curState; 
+    if (curState == 1){
+       digitalWrite(LED_BUILTIN,LOW);
+       sendMessage(); //Send message
+    } else {
+       digitalWrite(LED_BUILTIN,HIGH);
+    }
+  }
+  //servoposition = map(flexposition, 3, 400, 0, 180);
+  //servoposition = constrain(servoposition, 0, 180);
+
+
+  //servo1.write(servoposition);
+  
+  Serial.print("sensor: ");
+  Serial.println(flexposition);
+  //Serial.print("  servo: ");
+  //Serial.println(servoposition);
+
+
+  delay(20);  // wait 20ms between servo updates
+
+}
