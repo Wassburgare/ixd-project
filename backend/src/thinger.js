cba4acc5e3e2a3f2aabc7f550168cc07792600e9
Wassburgare <daniel.wassbjer@gmail.com>
2018-10-23 14:17:35 +0200

    Connect website to Thinger (kinda)

--- backend/src/thinger.js
+++ backend/src/thinger.js
@@ -1,18 +1,20 @@
 const request = require('request');
 
-const DEVICE_API_URL = 'XXX';
-const BEARER_TOKEN = 'YYY';
+const DEVICE_API_URL = 'http://localhost';
+const BEARER_TOKEN = '';
 
-const play = (message) => {
+const play = (note) => {
   // TODO: Replace with websocket, if possible
-  // request.post({
-  //   url: DEVICE_API_URL,
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     Authorization: `Bearer ${BEARER_TOKEN}`,
-  //   },
-  //   body: message,
-  // });
+  request.post({
+    url: DEVICE_API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${BEARER_TOKEN}`,
+    },
+    body: JSON.stringify({
+      message: `xylo${note}`,
+    }),
+  });
 };
 
 module.exports = {
