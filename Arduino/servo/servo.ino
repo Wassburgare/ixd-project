eaf84ee74ae4566c16b9c9104f0e37bb63de556e
clutchek <nalex.k@hotmail.com>
2018-10-23 15:14:32 +0200

    Refactored servo code

--- Arduino/servo/servo.ino
+++ Arduino/servo/servo.ino
@@ -48,12 +48,37 @@ const char *password = "Dprj18iot";
 
 const int arraylength = 6;
 
+
+typedef struct{
+  String instrumentName;
+  boolean instrumentActive;
+  boolean goingBackwards;
+  boolean messageReceived;
+  int servoMin;
+  int servoMax;
+  int servoDelay;
+  int instrumentTimer;
+  int servoPort;
+}instrument;
+
+instrument instruments[7] = {
+  {"drum1Down", false, false, false,450,500,100,0,11},
+  {"drum2Down", false, false, false,500,650,150,0,10},
+  {"xylo1", false, false, false,225,300,150,0,4},
+  {"xylo2", false, false, false,400,440,150,0,5},
+  {"xylo3", false, false, false,240,200,150,0,6},
+  {"xylo4", false, false, false,475,520,150,0,7},
+  {"guitar", false, false, false,150,300,200,0,15},
+};
+
+
+//drum1Down, drum1Up
 //Booleans representing whether a instrument is in motion,
 // Order: drum1, drum2, xylo1, xylo2, xylo3, xyol4
-boolean instrumentActive[arraylength] = {false, false, false, false, false, false};
+//boolean instrumentActive[arraylength] = {false, false, false, false, false, false};
 
 //Representing if each instrument has gotten a start message
-boolean messageReceived[arraylength] = {false, false, false, false, false, false};
+//boolean messageReceived[arraylength] = {false, false, false, false, false, false};
 
 //int servominimum[7] = {150,150,150,150,150,150,150};
 
@@ -63,16 +88,16 @@ boolean messageReceived[arraylength] = {false, false, false, false, false, false
 
 //Starting time for instruments motions in milliseconds
 // Order: drum1, drum2, xylo1, xylo2, xylo3, xyol4
-long instrumentTimer[arraylength] = {0,0,0,0,0,0};
+//long instrumentTimer[arraylength] = {0,0,0,0,0,0};
 
 //port numbers
-int servoPort[7] = {13,14,2,3,4,5,15};
+//int servoPort[7] = {13,14,2,3,4,5,15};
 
 //Guitar angle
 int guitarValue = SERVOMIN;
 
 //List containing messages from instruments
-String instrumentNames[arraylength] = {"drum1", "drum2", "xylo1", "xylo2", "xylo3", "xylo4"};
+//String instrumentNames[arraylength] = {"drum1", "drum2", "xylo1", "xylo2", "xylo3", "xylo4"};
 
 ThingerESP8266 thing(USER_ID, DEVICE_ID, DEVICE_CREDENTIAL);
 
@@ -118,43 +143,27 @@ void setup() {
   
   pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
 
-  //resetServos();
-
   delay(10);
-}
-
-// you can use this function if you'd like to set the pulse length in seconds
-// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
-void setServoPulse(uint8_t n, double pulse) {
-  double pulselength;
-  
-  pulselength = 1000000;   // 1,000,000 us per second
-  pulselength /= 60;   // 60 Hz
-  Serial.print(pulselength); Serial.println(" us per period"); 
-  pulselength /= 4096;  // 12 bits of resolution
-  Serial.print(pulselength); Serial.println(" us per bit"); 
-  pulse *= 1000000;  // convert to us
-  pulse /= pulselength;
-  Serial.println(pulse);
-  pwm.setPWM(n, 0, pulse);
 }
 
 //Function to read the received messages, probably needs the delays removed 
 void setupReceive(){
   thing["receive"] << [](pson& in){
+    
     String message = in["message"];
-    Serial.println(message);
+    //Serial.println(message);
 
     if(message == "guitar"){
       int value = in["guitarData"];
+      guitarValue = map(value, GUITARMIN, GUITARMAX, instruments[6].servoMin, instruments[6].servoMax);
+      
       //Serial.println("Value was :");
-      //Serial.println(value);
-      guitarValue = map(value, GUITARMIN, GUITARMAX, SERVOMIN, SERVOMAX);
+      //Serial.println(guitarValue);
     }else{
       for(int i = 0; i < arraylength; i++){
-        if(message == instrumentNames[i]){
-           if(!messageReceived[i]){
-              messageReceived[i] = true;
+        if(message == instruments[i].instrumentName){
+           if(!instruments[i].messageReceived){
+              instruments[i].messageReceived = true;
            }
         }
       }  
@@ -163,33 +172,36 @@ void setupReceive(){
 }
 
 
-void resetServos(){
+/*void resetServos(){
   for(int i = 0; i < (arraylength+1); i++){
-    pwm.setPWM(servoPort[i],0,300);
+    pwm.setPWM(instruments[i].servoPort,0,300);
   }
-}
+}*/
 
 void runServos(){  
   //Setting the value for the guitar
-  pwm.setPWM(servoPort[6], 0, guitarValue);
+  pwm.setPWM(instruments[6].servoPort, 0, guitarValue);
   
   for(int i = 0; i < arraylength; i++){
-    if(!instrumentActive[i] && messageReceived[i]){
-      //Serial.println("Instrument: " + instrumentNames[i]);
-      //Serial.println("Message received: " + messageReceived[i]);
-      //Serial.println("instrumentActive: " + instrumentActive[i]);
-      Serial.println(instrumentNames[i] + " Motor start");
+    if(!instruments[i].instrumentActive && instruments[i].messageReceived){
+      //Serial.println(instruments[i].instrumentName + " Motor start");
 
       
       //Inte aktiv, fått meddelande, starta motor och timer
-      pwm.setPWM(servoPort[i], 0, SERVOMAX);
-      instrumentActive[i] = true;
-      instrumentTimer[i] = millis() + SERVODELAY;
-    }else if(instrumentActive[i] && (millis() > instrumentTimer[i])){
-      Serial.println(instrumentNames[i] + " Motor off");
-      messageReceived[i] = false;
-      instrumentActive[i] = false;
-      pwm.setPWM(servoPort[i], 0, SERVOMIN);
+      pwm.setPWM(instruments[i].servoPort, 0, instruments[i].servoMax);
+      instruments[i].instrumentActive = true;
+      instruments[i].instrumentTimer = millis() + instruments[i].servoDelay;
+      
+    }else if(instruments[i].instrumentActive && (millis() > instruments[i].instrumentTimer) && !instruments[i].goingBackwards){
+      //Serial.println(instruments[i].instrumentName + " Motor off");
+      
+      pwm.setPWM(instruments[i].servoPort, 0, instruments[i].servoMin);
+      instruments[i].instrumentTimer = millis() + instruments[i].servoDelay;
+      instruments[i].goingBackwards = true;
+    }else if(instruments[i].goingBackwards && (millis() > instruments[i].instrumentTimer)){
+      //Wait until reset
+      instruments[i].instrumentActive = false;
+      instruments[i].goingBackwards = false;
     }
   }
 }
